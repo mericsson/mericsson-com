7d075a0cc30974b538407ffa2d0a986720f0a9ba
Marcus Ericsson <36717+mericsson@users.noreply.github.com>
2021-11-11 15:13:16 -0800

    add favs

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,29 +1,42 @@
 import React from "react"
 
+function makeLink(project) {
+  const { url, name, description } = project;
+  return (
+    <li key={name}>
+      <a href={url} title={description} target="_blank" rel="noopener noreferrer">{name}</a> - {description}
+    </li>
+  );
+}
+
+const projects = [{
+  url: 'https://segment.com',
+  name: 'Segment',
+  description: 'Eng Manager - Integrations.'
+},{
+  url: 'https://mixmax.com',
+  name: 'Mixmax',
+  description: 'Tech lead for Integrations team.'
+}, {
+  url: 'https://salesforce.com',
+  name: 'Salesforce',
+  description: 'Lead engineer on Community Cloud, Chatter.'
+}];
+
+const favorites = [{
+  url: 'https://themanagershandbook.com/',
+  name: 'The Manager\'s Handbook',
+  description: 'Guide to engineering management from Clearbit\'s CEO'
+}, {
+  url: 'https://staffeng.com/book',
+  name: 'Staff Engineer by Will Larson',
+  description: 'Guide to understanding and being a great Staff+ engineer'
+}]
+
 function Bio() {
 
-  const projects = [{
-    url: 'https://segment.com',
-    name: 'Segment',
-    description: 'Eng Manager - Integrations.'
-  },{
-    url: 'https://mixmax.com',
-    name: 'Mixmax',
-    description: 'Tech lead for Integrations team.'
-  }, {
-    url: 'https://salesforce.com',
-    name: 'Salesforce',
-    description: 'Lead engineer on Community Cloud, Chatter.'
-  }];
-  const projectLinks = projects
-    .map(project => {
-      const { url, name, description } = project;
-      return (
-        <li key={name}>
-          <a href={url} title={description} target="_blank" rel="noopener noreferrer">{name}</a> - {description}
-        </li>
-      );
-    });
+  const projectLinks = projects.map(makeLink)
+  const favoriteLinks = favorites.map(makeLink)
 
   return (
 
@@ -34,6 +47,10 @@ function Bio() {
       <h1 className="page-heading">Work</h1>
       <ul>
         {projectLinks}
+      </ul>
+      <h1 className="page-heading">Favorites</h1>
+      <ul>
+        {favoriteLinks}
       </ul>
     </div>
   )
